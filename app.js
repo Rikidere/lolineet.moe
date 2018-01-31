f7000da5a26ec8f3126cef11368e5a7578879a3e
Rikidere <rorikidesu@gmail.com>
2018-01-31 08:35:17 +0100

    format

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
+var routes = require('routes');
 var path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public_html')));
 
-app.listen(80, () => console.log('Example app listening on port 3001!'));
+app.listen(80, () => console.log('Example app listening on port 80!'));

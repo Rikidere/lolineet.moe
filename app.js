f7b8e05ed7692c46866efa42b42581fb2bb425c6
Rikidere <rorikidesu@gmail.com>
2018-01-31 07:53:49 +0100

    Simple Placeholder

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Placeholder for future website");
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public_html')));
+
+app.listen(80, () => console.log('Example app listening on port 3001!'));
